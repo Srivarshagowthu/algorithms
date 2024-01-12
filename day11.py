0488d2217ab27d535689a40ca701bf957296f81a
Srivarsha gowthu <103606595+Srivarshagowthu@users.noreply.github.com>
2024-01-12 21:50:05 +0530

    Updated day11.py

--- day11.py
+++ day11.py
@@ -54,7 +54,46 @@ def seg(l,r):
 l=10
 r=50
 print(seg(l,r))
-    
+######################################################################
+#prime in a range using seive and segmented
+import math
+class Solution:        
+    def primeRange(self,M,N):
+        #code here
+        def seive(n):
+            q=[]
+            l=[True for i in range(n+1)]
+            p=2
+            while(p*p<=n):
+                if l[p]:
+                    for i in range(p*p,n+1,p):
+                        l[i]=False
+                p+=1
+            for j in range(2,n+1):
+                if l[j]:
+                    q.append(j)
+            return q
+        def seg(l,r):
+            ans=[]
+            k=int(math.floor(math.sqrt(r))+1)#4
+            primes=seive(k)
+            array=[True for i in range(r-l+1)]
+            for j in (primes):
+                w=(l//j)*j
+                if(w<l):
+                    w+=j
+                if(j*j > w): w = j* j;#
+                while(w<=r):
+                    array[w-l]=False
+                    w+=j
+            for b in range(l,r+1):
+                if array[b-l]:
+                    ans.append(b)
+            if l==1:
+                return ans[1::]
+            else:
+                return ans
+Note: no need to use segmented seive if the constraints aee under 10**6
                 
     
     
